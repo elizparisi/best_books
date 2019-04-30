ed0148c169c0e0297740d0f671528f6e33b4ee91
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-04-30 05:35:40 +0000

    updated scraper

--- lib/best_books/scraper.rb
+++ lib/best_books/scraper.rb
@@ -1,10 +1,9 @@
 class Scraper
   
   def self.scrape_page
-    html = open("https://www.goodreads.com/book/popular_by_date/2019")
-    doc = Nokogiri::HTML(html)
+    doc = Nokogiri::HTML(open("https://www.goodreads.com/book/popular_by_date/2019"))
     
-    doc.css("div.leftContainer").each do |book_css|
+    doc.css("table.tableList tr").each do |book_css|
       book = Book.new 
       book.title = book_css.css("a.bookTitle span").text.strip
       book.author = book_css.css("a.authorName").text.strip
